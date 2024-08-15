89c5dd6fad55f9ca2aa0ea9040b61ba411b59299
rafaseoanes <rafaseoanes9@gmail.com>
2024-08-15 15:45:48 -0500

    Hero section created and styled

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -1,12 +1,31 @@
-import profileImg from "./assets/profileImg.png";
-import "./Hero.css"
+import { useEffect, useState } from "react";
 
+import profileImg from "./assets/profileImg.png";
+import "./Hero.css";
+import sign from "./assets/sign.png";
 
 function Hero() {
+  const [loadAnimationName, setLoadAnimationName] = useState("nameStart");
+  const [loadAnimationTitle, setLoadAnimationTitle] = useState("titleStart");
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoadAnimationName("name");
+      setLoadAnimationTitle("title")
+    }, "1000");
+
+   
+  });
+
   return (
     <>
       <div className="profileImgContainer">
-        <img src={profileImg} className="profilePhoto" alt="Profile photo" />
+        <div className="circle orangeCircle"></div>
+        <div className="circle mintCircle"></div>
+        <h1 className={loadAnimationName}>Rafael Seoanes</h1>
+        {/* <img src={profileImg} className="profilePhoto" alt="Profile photo" /> */}
+        <h2 className={loadAnimationTitle}>Frontend Developer</h2>
+        {/* <img src={sign} className="signImg" alt="Name" /> */}
       </div>
     </>
   );
