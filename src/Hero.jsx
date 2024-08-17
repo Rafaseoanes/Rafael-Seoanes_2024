48f6ff0af3241807cb6da7ead7b2803e26c18daf
rafaseoanes <rafaseoanes9@gmail.com>
2024-08-16 20:43:20 -0500

    Hero animations added

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -3,19 +3,22 @@ import { useEffect, useState } from "react";
 import profileImg from "./assets/profileImg.png";
 import "./Hero.css";
 import sign from "./assets/sign.png";
+import arrow from "./assets/arrow.png";
 
 function Hero() {
   const [loadAnimationName, setLoadAnimationName] = useState("nameStart");
   const [loadAnimationTitle, setLoadAnimationTitle] = useState("titleStart");
+  const [hideArrow, setHideArrow] = useState("hideArrow");
 
   useEffect(() => {
     setTimeout(() => {
       setLoadAnimationName("name");
-      setLoadAnimationTitle("title")
+      setLoadAnimationTitle("title");
     }, "1000");
-
-   
   });
+  setTimeout(() => {
+    setHideArrow("arrow");
+  }, "2000");
 
   return (
     <>
@@ -26,6 +29,7 @@ function Hero() {
         {/* <img src={profileImg} className="profilePhoto" alt="Profile photo" /> */}
         <h2 className={loadAnimationTitle}>Frontend Developer</h2>
         {/* <img src={sign} className="signImg" alt="Name" /> */}
+        <img src={arrow} className={hideArrow} alt="arrow" />
       </div>
     </>
   );
