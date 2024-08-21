34903f50bdc039d13ec059969c74e3915b9c61b6
rafaseoanes <rafaseoanes9@gmail.com>
2024-08-21 14:52:26 -0500

    Menu bar addet to the page, general design working perfectly

---
+++ src/Menu.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Menu.css";
+
+function Menu() {
+  return (
+    <div className="menu">
+      <h4 className="menuName">Rafael Seoanes</h4>
+      <ul>
+        <li>Home</li>
+        <li>Projects</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Menu;
--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,13 @@ import { createRoot } from "react-dom/client";
 
 import About from "./About.jsx";
 import Hero from "./hero.jsx";
+import Menu from "./Menu.jsx";
 
 import "./index.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-   
+    <Menu />
     <Hero />
     <About />
   </StrictMode>
