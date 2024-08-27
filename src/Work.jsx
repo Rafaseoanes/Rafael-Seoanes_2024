fc7ac8426cad0bb75fcb1733c14a20f4f90f85d7
rafaseoanes <rafaseoanes9@gmail.com>
2024-08-27 15:34:16 -0500

    scroll animation addect to the projects section

--- src/Work.jsx
+++ src/Work.jsx
@@ -1,17 +1,21 @@
-import React from "react";
+import { useInView } from "react-intersection-observer";
 import Project from "./Project";
 import "./Work.css";
 
 function Work() {
+  const { ref, inView } = useInView({
+    threshold: 0,
+  });
+
   return (
-    <div className="workSection">
-      <div className="leftColumn">
+    <div ref={ref} className="workSection">
+      <div className={inView ? "leftColumn  animate" : "leftColumn"}>
         <h3 className="projectsTitle">Projects</h3>
         <Project />
         <Project />
         <Project />
       </div>
-      <div className="rightColumn">
+      <div className={inView ? "rightColumn  animate" : "rightColumn"}>
         <Project />
         <Project />
         <Project />
